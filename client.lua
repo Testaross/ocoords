6bece9370326d4a41c0433e74c6fe080effe0c0c
Testarossa <austin.w.burrows@gmail.com>
2022-12-19 17:32:38 -0500

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,18 +1,13 @@
 
 RegisterCommand('c', function(source, args, rawCommand)
 	local coords = GetEntityCoords(PlayerPedId())
-	SendNUIMessage({
-		coords = ""..coords.x..","..coords.y..","..coords.z..""
-	})
-	-- exports['mythic_notify']:SendAlert('success', 'Coords saved')
+	lib.setClipboard(""..coords.x..","..coords.y..","..coords.z.."")
 end)
 
 
 RegisterCommand('ch', function(source, args, rawCommand)
 	local coords = GetEntityCoords(PlayerPedId())
 	local heading = GetEntityHeading(PlayerPedId())
-	SendNUIMessage({
-		coords = ""..coords.x..","..coords.y..","..coords.z..","..heading..""
-	})
+	lib.setClipboard(""..coords.x..","..coords.y..","..coords.z..","..heading.."")
 end)
 
