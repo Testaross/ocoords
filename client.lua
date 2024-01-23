eebdd75212e01657f90f0788bdea9144fc1cc4fb
Testarossa <austin.w.burrows@gmail.com>
2024-01-22 20:33:31 -0500

    fixxxxxed

--- client.lua
+++ client.lua
@@ -1,13 +1,12 @@
-ped = PlayerPedId()
-
-RegisterCommand('c', function(source, args, rawCommand)
-	local coords = GetEntityCoords(ped)
+RegisterCommand('c', function()
+	local coords = GetEntityCoords(cache.ped)
+	print(coords)
 	lib.setClipboard(tostring(coords))
 end)
 
 
-RegisterCommand('ch', function(source, args, rawCommand)
-	local coords = GetEntityCoords(ped)
+RegisterCommand('ch', function()
+	local coords = GetEntityCoords(cache.ped)
 	local heading = GetEntityHeading(ped)
 	lib.setClipboard(tostring("" ..coords.x.."," ..coords.y.."," ..coords.z.."," ..heading..""))
 end)
